da26ce39e396ff0e47d190824dbda9df29c16300
Elia <elia.renzoni03@gmail.com>
2024-11-21 11:25:15 +0100

    add some tests

--- storage/internal/resource_controller_test.go
+++ storage/internal/resource_controller_test.go
@@ -12,3 +12,28 @@ func TestCreateNewDir(t *testing.T) {
 	}
 }
 
+func TestDeleteDir(t *testing.T) {
+	controller := &internal.ResourceController{}
+
+	controller.CreateNewDir("test_dir2")
+
+	if err := controller.DeleteDir("test_dir2"); err != nil {
+		t.Errorf("%v", err)
+	}
+}
+
+func TestWriteRemoteCSV(t *testing.T) {
+	controller := &internal.ResourceController{}
+	var query = []string{
+		"ID",
+		"Name",
+		"Surname",
+	}
+
+	controller.CreateNewDir("test_dir2")
+
+	if err := controller.WriteRemoteCSV("test_dir2", "test_file.csv", "insert", query); err != nil {
+		t.Errorf("%v", err) // fail
+	}
+} 
+
