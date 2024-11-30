6c4d23e721e863d05a7d1023e54eeda2c5168fa6
Elia <elia.renzoni03@gmail.com>
2024-11-30 16:58:33 +0100

    update index.js

--- service/index.js
+++ service/index.js
@@ -1,12 +1,13 @@
-// static server
+// static file server for MessDFS
 
 const express = require('express')
 const app = express()
 const listenPort = 3000
 
-app.get("/", function (req, res) {
-    res.send("Hello World!")
-});
+app.use("/index", express.static('public/index.html'))
+app.use("/login", express.static('public/login.html'))
+app.use("/signup", express.static('public/signup.html'))
+app.use("/signout", express.static('public/signout.html'))
 
 app.listen(listenPort, function() {
     console.log("Server Listening...")
