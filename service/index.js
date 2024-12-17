16ec2db815e763ee7a94095419150b25c1c87bfb
Elia <elia.renzoni03@gmail.com>
2024-12-17 17:56:42 +0100

    update service microservice

--- service/index.js
+++ service/index.js
@@ -4,10 +4,11 @@ const express = require('express')
 const app = express()
 const listenPort = 3000
 
-app.use("/index", express.static('public/index.html'))
-app.use("/login", express.static('public/login.html'))
-app.use("/signup", express.static('public/signup.html'))
-app.use("/signout", express.static('public/signout.html'))
+app.use(express.static("public"));
+app.use("/index", express.static('index.html'))
+app.use("/login", express.static('login.html'))
+app.use("/signup", express.static('signup.html'))
+app.use("/signout", express.static('signout.html'))
 
 app.listen(listenPort, function() {
     console.log("Server Listening...")
