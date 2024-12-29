131e5e4df04a8c12d5475b8db1506237f345cd9a
elia renzoni <elia.renzoni03@gmail.com>
2024-12-29 16:00:34 +0100

    add class fieldss

--- auth/app/router.py
+++ auth/app/router.py
@@ -4,13 +4,18 @@
 
 import socket
 import threading
+import login
+import signout
+import signup
 
 PORT = 8082
 HOST = '172.0.0.1'
 
 class Router:
     def __init__(self):
-        pass
+        self.login = login.Login()
+        self.signout = signout.Signout()
+        self.signup = signup.Signup()
 
     def start_handler(self):
         pass
