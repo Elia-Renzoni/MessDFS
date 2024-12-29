8e9b544c573099dc7091fb98588df57f8cc369c8
elia renzoni <elia.renzoni03@gmail.com>
2024-12-29 16:11:38 +0100

    create Router init body content

--- auth/app/router.py
+++ auth/app/router.py
@@ -8,14 +8,14 @@ import login
 import signout
 import signup
 
-PORT = 8082
-HOST = '172.0.0.1'
-
 class Router:
-    def __init__(self):
+    def __init__(self, host, listen_port):
+        self.host = host
+        self.port = listen_port
         self.login = login.Login()
         self.signout = signout.Signout()
         self.signup = signup.Signup()
+        self.conn = socket.create_server((self.host, self.port))
 
     def start_handler(self):
         pass
