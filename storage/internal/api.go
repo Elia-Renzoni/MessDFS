25c1dee4c0c7d7ab2dc9fef4f145aca29912f020
Elia <elia.renzoni03@gmail.com>
2025-01-29 16:54:28 +0100

    update api

--- storage/internal/api.go
+++ storage/internal/api.go
@@ -69,8 +69,9 @@ func (m *MessDFSStorageAPI) deleteCSV(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	splittedPath := strings.Split(path, "/")
 
-	m.delete.User = splittedPath[2]
-	m.delete.FileName = splittedPath[3]
+	m.delete.TransactionUser = splittedPath[2]
+	m.delete.User = splittedPath[3]
+	m.delete.FileName = splittedPath[4]
 	m.delete.Query = r.URL.Query()
 
 	if err := m.DeleteRemoteCSV(m.delete.User, m.delete.FileName, "delete", m.delete.Query); err != nil {
@@ -88,8 +89,9 @@ func (m *MessDFSStorageAPI) readCSV(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	splittedPath := strings.Split(path, "/")
 
-	m.read.User = splittedPath[2]
-	m.read.FileName = splittedPath[3]
+	m.read.TransactionUser = splittedPath[2]
+	m.read.User = splittedPath[3]
+	m.read.FileName = splittedPath[4]
 	m.read.Query = r.URL.Query()
 
 	fmt.Printf("%s - %s - %s \n", m.read.User, m.read.FileName, m.read.Query)
@@ -137,7 +139,8 @@ func (m *MessDFSStorageAPI) createDirectory(w http.ResponseWriter, r *http.Reque
 func (m *MessDFSStorageAPI) deleteDirectory(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	splittedPath := strings.Split(path, "/")
-	m.deleteDir.DirToDelete = splittedPath[2]
+	m.deleteDir.TransactionUser = splittedPath[2]
+	m.deleteDir.DirToDelete = splittedPath[3]
 
 	if err := m.DeleteDir(m.deleteDir.DirToDelete); err != nil {
 		http.Error(w, err.Error(), 500)
@@ -153,8 +156,9 @@ func (m *MessDFSStorageAPI) deleteDirectory(w http.ResponseWriter, r *http.Reque
 func (m *MessDFSStorageAPI) deleteFile(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	splittedPath := strings.Split(path, "/")
-	m.deleteF.DirName = splittedPath[2]
-	m.deleteF.FileToDelete = splittedPath[3]
+	m.deleteFile.TransactionUser = splittedPath[2]
+	m.deleteF.DirName = splittedPath[3]
+	m.deleteF.FileToDelete = splittedPath[4]
 
 	if err := m.DeleteFile(m.deleteF.DirName, m.deleteF.FileToDelete); err != nil {
 		http.Error(w, err.Error(), 500)
