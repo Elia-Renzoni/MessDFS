d0b2c1df2fa3a3efcecc4ef265bd49933d2fde4e
Elia <elia.renzoni03@gmail.com>
2025-02-06 16:43:46 +0100

    update app

--- auth/app/router.py
+++ auth/app/router.py
@@ -9,18 +9,14 @@ from psycopg2 import Error
 import json
 from urllib.parse import urlparse, parse_qs
 
+TO_COMMIT = 1
+READ = 0
+
 class Router(BaseHTTPRequestHandler):
-    # in case of HTTP 1.1
-    def do_OPTIONS(self):
-        self.send_response(200)
-        self.send_header("Access-Control-Allow-Origin", "*")  
-        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-        self.send_header("Access-Control-Allow-Headers", "Content-Type")
-        self.end_headers()
-    
     def do_POST(self):
         parsed = urlparse(self.path)
-        match parsed:
+        print(parsed)
+        match parsed.path:
             case "/signup":
                 self.signup()
             case "/add-friend":
@@ -47,22 +43,13 @@ class Router(BaseHTTPRequestHandler):
             case _: 
                 self.write_bad_request()
     
-    def do_DELETE(self):
-        parsed = urlparse(self.path)
-        match parsed:
-            case "/signout": 
-                self.signout()
-            case _: 
-                self.write_bad_request()
-
-    
     def login(self):
         print("login", file=sys.stdout)
         parsed_url = urlparse(self.path)
         parsed_query = parse_qs(parsed_url.query)
         username = parsed_query.get("username")
         password = parsed_query.get("password")
-        result = self.connect_db("SELECT username, password FROM users WHERE username = '%s' AND password = '%s'" % (username.pop(), password.pop()))
+        result = self.connect_db("SELECT username, password FROM users WHERE username = '%s' AND password = '%s'" % (username.pop(), password.pop()), READ)
         if len(result) == 0:
             self.send_response(400)
         else:
@@ -71,17 +58,20 @@ class Router(BaseHTTPRequestHandler):
     
     def signup(self):
         print("signup", file=sys.stdout)
-        self.send_response(200)  # Codice HTTP 200 OK
-        self.send_header("Content-Type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Signup endpoint reached")
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        deser_data = json.loads(body.decode('UTF-8'))
+        username = deser_data["username"]
+        password = deser_data["password"]
+        
+        result = self.connect_db("INSERT INTO users (username, password) VALUES ('%s', '%s')" % (username, password), TO_COMMIT)
+        if result != None:
+            self.send_response(400)
+            self.end_headers()
+        else: 
+            self.send_response(201)
+            self.end_headers()
     
-    def signout(self):
-        print("signout", file=sys.stdout)
-        self.send_response(200)  # Codice HTTP 200 OK
-        self.send_header("Content-Type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Signout endpoint reached")
 
     def add_friend(self):
         print("add-friend", file=sys.stdout)
@@ -91,11 +81,14 @@ class Router(BaseHTTPRequestHandler):
         username = deser_data["username"]
         friend_name = deser_data["friend_username"]
 
-        result = self.connect_db("INSERT INTO friends (username, friend) VALUES ('%s', '%s')" % (username.pop(), friend_name.pop()))
-        if len(result) != 0: 
+        result = self.connect_db("INSERT INTO friends (username, friend) VALUES ('%s', '%s')" % (username, friend_name), TO_COMMIT)
+        print(result)
+        if result != None: 
             self.send_response(400)
+            self.end_headers()
         else: 
             self.send_response(201)
+            self.end_headers()
         
     
     def add_directory(self):
@@ -106,11 +99,13 @@ class Router(BaseHTTPRequestHandler):
         username = deser_data_add_directory["username"]
         directory_name = deser_data_add_directory["directory"]
 
-        result = self.connect_db("INSERT INTO directories (username, directory) VALUES ('%s', '%s');" % (username.pop(), directory_name.pop()))
-        if len(result) != 0: 
+        result = self.connect_db("INSERT INTO directories (username, directory) VALUES ('%s', '%s');" % (username, directory_name), TO_COMMIT)
+        if result != None: 
             self.send_response(400)
+            self.end_headers()
         else: 
-            self.send_response(201)        
+            self.send_response(201)
+            self.end_headers()        
 
 
     ''''
@@ -123,7 +118,7 @@ class Router(BaseHTTPRequestHandler):
         parsed_query = parse_qs(parsed_url.query)
         txn = parsed_query.get("txn")
         directory = parsed_query.get("dir")
-        result = self.connect_db("SELECT username, friend FROM friends WHERE username = '%s' AND friend = '%s'" % (txn.pop(), directory.pop()))
+        result = self.connect_db("SELECT username, friend FROM friends WHERE username = '%s' AND friend = '%s'" % (txn.pop(), directory.pop()), READ)
         if len(result) == 0:
             self.send_response(400)
             self.end_headers()
@@ -136,7 +131,7 @@ class Router(BaseHTTPRequestHandler):
         parsed_url = urlparse(self.path)
         parsed_query = parse_qs(parsed_url.query)
         username = parsed_query.get("username")
-        result = self.connect_db("SELECT directory FROM directories WHERE username='%s'" % username.pop())
+        result = self.connect_db("SELECT directory FROM directories WHERE username='%s'" % username.pop(), READ)
         if len(result) == 0:
             self.send_response(400)
             self.end_headers()
@@ -154,7 +149,7 @@ class Router(BaseHTTPRequestHandler):
         parsed_url = urlparse(self.path)
         parsed_query = parse_qs(parsed_url.query)
         username = parsed_query.get("username")
-        result = self.connect_db("SELECT friend FROM friends WHERE username='%s'" % username.pop())
+        result = self.connect_db("SELECT friend FROM friends WHERE username='%s'" % username.pop(), READ)
         if len(result) == 0:
             self.send_response(400)
             self.end_headers()
@@ -177,7 +172,7 @@ class Router(BaseHTTPRequestHandler):
         parsed_url = urlparse(self.path)
         parsed_query = parse_qs(parsed_url.query)
         username = parsed_query.get("username")
-        result = self.connect_db("SELECT username FROM users WHERE username='%s'" % username.pop())
+        result = self.connect_db("SELECT username FROM users WHERE username='%s'" % username.pop(), READ)
         if len(result) == 0:
             self.send_response(404)
             self.end_headers()
@@ -194,7 +189,7 @@ class Router(BaseHTTPRequestHandler):
         self.send_response(400)
         self.wfile.write(b"You Wrote a Bad Request Buddy!")
     
-    def connect_db(self, sql_statement):
+    def connect_db(self, sql_statement, opcode):
         sql_results = None
         try:
             connection = psycopg2.connect(
@@ -206,7 +201,14 @@ class Router(BaseHTTPRequestHandler):
 
             cursor_obj = connection.cursor()
             cursor_obj.execute(sql_statement)
-            sql_results = cursor_obj.fetchall()
+
+            match opcode:
+                # SELECT statement
+                case 0:
+                    sql_results = cursor_obj.fetchall()
+                # WRITE statement
+                case 1:
+                    connection.commit()
             connection.close()
         except (Exception, Error) as error: 
             print('Occured Some Errors ', error)
--- auth/server.py
+++ auth/server.py
@@ -5,8 +5,8 @@
 from app.router import Router
 from http.server import HTTPServer
 
-def main(server_class=HTTPServer, handler_class=Router, port=8082):
-    server_addr = ('127.0.0.1', 8082)
+def main(server_class=HTTPServer, handler_class=Router, port=8083):
+    server_addr = ('127.0.0.1', 8083)
     httpd = server_class(server_addr, handler_class)
     print("Server Listening...")
     httpd.serve_forever()
